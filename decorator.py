d91f544d2b1440920eef6a58662ae5b9c1d9a867
eccentricayman <aahmed3@stuy.edu>
2017-04-28 12:07:40 -0400

    fibernacc

--- decorator.py
+++ decorator.py
@@ -18,13 +18,20 @@ def getTime( func ):
         
     return innerFunc
 
+def fibonacci(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return F(n-1)+F(n-2)
+
 def testFunc(x):
     time.sleep(1)
     return 5 * x
 
 timetest = getTime( testFunc )
-print timetest(5)
+#print timetest(5)
 
 infograb = getInfo(testFunc)
-print infograb(4)
-    
+#print infograb(4)
+
+informationaoinwdoiwan = getTime(fibonacci)
+informationaoinwdoiwan(700)
